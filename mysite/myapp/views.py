c6887b620e7f1dfa05b4e0a6824bb6462e96fb01
tkda-h3 <3791992498@edu.k.u-tokyo.ac.jp>
2016-10-16 03:47:27 +0900

    add template and form

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-# Create your views here.
+def hello_forms(request):
+    form = forms.HelloForm(request.GET or None)
+    if form.is_valid():
+        message = 'データ検証に成功しました'
+    else:
+        message = 'データ検証に失敗しました'
+    d = {
+        'form': form,
+        'message': message,
+    }
+    return render(request, 'forms.html', d)
+
+
