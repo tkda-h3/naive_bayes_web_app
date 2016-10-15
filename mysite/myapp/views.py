490de0f024470a28025d4cfba65c669b3c4d50f4
tkda-h3 <3791992498@edu.k.u-tokyo.ac.jp>
2016-10-16 06:35:45 +0900

    predict categry name

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-# from . import forms
 
+from prettyprint import pp
 import pickle
 import urllib2
 from bs4 import BeautifulSoup
 import MeCab
 from gensim import corpora, matutils
 import numpy as np
+import os.path
 
 def get_obj_from_pickle(file_path):
     with open(file_path, 'r') as f:
@@ -30,6 +31,13 @@ def get_words_list(text):
         node = node.next
     return words_list
 
+def calc_log_prob(word_cat_prob, cat_prob, doc): # doc : 1 feature vector
+        doc_word_index = np.where(doc != 0)
+        each_word_prob_in_doc  = word_cat_prob[:, doc_word_index[1]] #docに含まれる単語の各P(word | cat)
+        each_log_word_cat_prob = np.log(each_word_prob_in_doc).sum(axis=1)
+        ans = np.log(cat_prob) + each_log_word_cat_prob
+        return ans
+
 
 def predict_y_of_input_url(input_url, dictionary,word_cat_prob, cat_prob):
     text_of_input_url = get_content(input_url)
@@ -41,27 +49,25 @@ def predict_y_of_input_url(input_url, dictionary,word_cat_prob, cat_prob):
 
 def get_category_name(url):
     # deserialize
-    dir_path = '/Users/takada/desktop/naive_bayes_web_app/'
+    dir_path = os.path.abspath('./../')+'/'
     cat_prob = get_obj_from_pickle(dir_path+'cat_prob.dump')
     word_cat_prob = get_obj_from_pickle(dir_path+'word_cat_prob.dump')
     dictionary = get_obj_from_pickle(dir_path+'dictionay.dump')
     cvt_y_to_category_name = get_obj_from_pickle(dir_path+'cvt_y_to_category_name.dump')
-    predict_y_of_input_url(url, dictionary, word_cat_prob, cat_prob)
-    
-
-
-
-
+    pred_y_of_input_url = predict_y_of_input_url(url, dictionary, word_cat_prob, cat_prob)
+    pred_category_name_of_input_url = cvt_y_to_category_name[pred_y_of_input_url] #数字ラベルを変換
+    return pred_category_name_of_input_url
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def hello_forms(request):
+def hello_forms(request): 
     d = {
-        'input_url': request.GET.get('input_url')
+        'input_url': request.GET.get('input_url'),
     }
-    
-    get_category_name(d['input_url'])
+    if d['input_url']:
+        d['pred_category_name'] = get_category_name(request.GET.get('input_url'))
+    #get_category_name(request.GET.get('input_url'))
     
     return render(request, 'forms.html', d)
 
