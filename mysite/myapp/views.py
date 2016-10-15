a4d7eeb3f274771c9ae2c1d18f6de4f37db5588b
tkda-h3 <3791992498@edu.k.u-tokyo.ac.jp>
2016-10-16 04:13:27 +0900

    change form action

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import forms
+# from . import forms
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def hello_forms(request):
-    form = forms.HelloForm(request.GET or None)
-    if form.is_valid():
-        message = 'データ検証に成功しました'
-    else:
-        message = 'データ検証に失敗しました'
     d = {
-        'form': form,
-        'message': message,
+        'your_name': request.GET.get('your_name')
     }
     return render(request, 'forms.html', d)
 
